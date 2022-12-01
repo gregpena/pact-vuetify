ec84c2c0f18bc0f3b0a149b1c3f42fb46617196a
GREGORY PENA <gregpena@hotmail.com>
2022-12-01 09:19:35 -0800

    Initial commit

---
+++ resources/js/Components/Checkbox.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:checked']);
+
+const props = defineProps({
+    checked: {
+        type: [Array, Boolean],
+        default: false,
+    },
+    value: {
+        default: null,
+    },
+});
+
+const proxyChecked = computed({
+    get() {
+        return props.checked;
+    },
+
+    set(val) {
+        emit("update:checked", val);
+    },
+});
+</script>
+
+<template>
+    <input type="checkbox" :value="value" v-model="proxyChecked"
+           class="tw-rounded tw-border-gray-300 tw-text-indigo-600 tw-shadow-sm focus:tw-border-indigo-300 focus:tw-ring focus:tw-ring-indigo-200 focus:tw-ring-opacity-50">
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="tw-font-semibold tw-text-xl tw-text-gray-800 tw-leading-tight">
+                Dashboard Test
+            </h2>
+        </template>
+
+        <div class="tw-py-12">
+            <div class="tw-max-w-7xl tw-mx-auto sm:tw-px-6 lg:tw-px-8">
+                <div class="tw-bg-white tw-overflow-hidden tw-shadow-sm sm:tw-rounded-lg">
+                    <div class="tw-p-6 tw-bg-white tw-border-b tw-border-gray-200">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
